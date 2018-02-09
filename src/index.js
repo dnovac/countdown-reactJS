8d0024e132a276beb722bcf1575523e75b81af0e
dnovac <danynovac@gmail.com>
2018-02-10 00:00:32 +0200

    Countdown App in ReactJS from Udemy tut

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dan on 03/02/2018.
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
